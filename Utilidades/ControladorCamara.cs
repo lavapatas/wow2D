dfb904f644c4e0f9e8b48a5e15bbb1c332b10d72
Pablxxs <andalavatelaspatas@yahoo.com>
2017-02-26 23:45:56 -0400

    camara casi terminada

--- Utilidades/ControladorCamara.cs
+++ Utilidades/ControladorCamara.cs
@@ -4,9 +4,43 @@ public class ControladorCamara{
 
    public GameObject objetivoASeguir;
    public float velocidadMovimiento;
-   public float posicionObjetivo;
+   public Vector3 posicionObjetivo;
+   public BoxCollider2D limitesColisionCamara;
+   
+   public static bool camaraExiste;
+   
+   private Vector3 bordeMaximo;
+   private Vector3 bordeMinimo;
+   private float mitadAltura;
+   private float mitadAnchura;
+   
+   private Camera camara;
+   
+   void Start(){
+     DontDestroyOnLoad(transform.gameObject)
+     
+     if(!camaraExiste){
+        camaraExiste=true;
+        DontDestroyOnLoad(transform.gameObject);
+     }else{
+     Destroy(gameObject);
+     }
+     
+     bordeMaximo = limitesColisionCamara.bounds.max;
+     bordeMinimo = limitesColisionCamara.bounds.min;
+     
+     mitadAltura = camara.orthographicSize;
+     mitadAnchura = mitadAltura* Screen.width/Screen.height;
+   }
    
    void Update(){
-      transform.position = Vector3.Lerp();
+      posicionObjetivo = new Vector3(objetivoASeguir.transform.position.x,objetivoASeguir.transform.position.y,transform.position.z);
+      transform.position = Vector3.Lerp(transform.positiom, posicionObjetivo, velocidadMovimiento*Time.deltaTime);
+      
+      float clampearX = Mathf.Clamp(transform.position.x, bordeMinimo.x + mitadAnchura, bordeMaximo.x - mitadAnchura );
+      
+      float clampearY = Mathf.Clamp(transform.position.y, bordeMinimo.y + mitadAltura, bordeMaximo.y - mitadAltura );
+      
+      transform.position = new Vector(clampearX, clampearY, transform.position.z);
    }
 }
--- Utilidades/ControladorMovimientoJugador.cs
+++ Utilidades/ControladorMovimientoJugador.cs
@@ -1,17 +1,17 @@
 using UnityEngine;
 
 public class ControladorMovimientoJugador(
-	public float velocidadMovimiento;
+	public float velocidadMovimientoJugador;
 	
 	void Start(){
 	}
 	
 	void Update(){
 		if(Input.GetAxisRaw("Horizontal")>0.2f || Input.GetAxisRaw("Horizontal") <0.2f) {
-				transform.translate(new Vector2(Input.GetAxisRaw("Horizontal")*velocidadMovimiento*Time.deltaTime),0f);
+				transform.translate(new Vector2(Input.GetAxisRaw("Horizontal")*velocidadMovimientoJugador*Time.deltaTime),0f);
 			}
 	if(Input.GetAxisRaw("Vertical")>0.2f || Input.GetAxisRaw("Vertical") <0.2f) {
-				transform.translate(new Vector2(Input.GetAxisRaw("Vertical")*velocidadMovimiento*Time.deltaTime),0f);
+				transform.translate(new Vector2(0f, Input.GetAxisRaw("Vertical")*velocidadMovimientoJugador*Time.deltaTime));
 			}
 		}
 			
