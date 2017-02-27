28b0c032847c30a5c79e58f888fa6c903217c32c
Pablxxs <andalavatelaspatas@yahoo.com>
2017-02-26 22:14:31 -0400

    Commit de utilidades

---
+++ Utilidades/ControladorCamara.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class ControladorCamara{
+
+   public GameObject objetivoASeguir;
+   public float velocidadMovimiento;
+   public float posicionObjetivo;
+   
+   void Update(){
+      transform.position = Vector3.Lerp();
+   }
+}
